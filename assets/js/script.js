16286ddadfb7151c337e6796e8a268dabb8aae76
Thomas Rider <thomasjrideriii@gmail.com>
2020-05-15 12:45:56 -0400

    Added button responsiveness and wiki data output

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,44 @@
-var queryURL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles="
+var hobby = "";
 
-// var hobby = "Underwater Basket Weaving"
-var hobby = "Mountain Biking"
+$("#hobbyDropdown").change(function(){
+    
+    var hobbyName = $(this).find("option:selected").text()
+    console.log($(this))
+    console.log(hobbyName)
 
-$.ajax({
-    url: queryURL + hobby + "&origin=*",
-    method: "GET"
-}).then(function(result){
-    // console.log(result)
+    if (hobbyName === "Select") {
+        var hobbyName = "Hobby"
+        
+    } 
+    
+    wikiData(hobbyName)
+})
+
+
+
+
+
+var queryURL =
+  "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info%7Cextracts&redirects=1&inprop=url&exintro=1&explaintext=1&titles=";
+// prop=extracts&exintro&explaintext&
+
+// /w/api.php?action=query&format=json&prop=info%7Cextracts&redirects=1&inprop=url&exintro=1&explaintext=1&titles=
+// /w/api.php?format=json&action=query&prop=info&inprop=url&prop=extracts&exintro&explaintext&redirects=1&titles=
+function wikiData(hobbyName) {
+  $.ajax({
+    url: queryURL + hobbyName + "&origin=*",
+    method: "GET",
+  }).then(function (result) {
+    console.log(result)
     // console.log(result.query.pages)
     // console.log(Object.keys(result.query.pages))
-    var pageNum = Object.keys(result.query.pages)
+    var pageNum = Object.keys(result.query.pages);
     // console.log(result.query.pages[pageNum[0]])
-    var summaryText = result.query.pages[pageNum[0]].extract
-    // console.log(summaryText)
-    var newPar = $("<p>")
-    newPar.text(summaryText)
-    $("#summaryText").prepend(newPar)
-})
+    var summaryText = result.query.pages[pageNum[0]].extract;
+    var pageLink = result.query.pages[pageNum[0]].canonicalurl;
+    console.log(summaryText)
+    $("#wikiLink").attr("href", pageLink);
+    $("#hobbyName").text(hobbyName)
+    $("#summaryText").text(summaryText);
+  });
+}
